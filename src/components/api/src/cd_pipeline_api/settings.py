0273c8aa9c401c942b288eda932282548d0fba27
Nathan Freeman <nathan.dean.freeman@gmail.com>
2022-04-08 15:03:37 -0500

    update allowed hosts in api

--- src/components/api/src/cd_pipeline_api/settings.py
+++ src/components/api/src/cd_pipeline_api/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-_!+8ppjzrs!$8l5n2k)^ry2=c&jkrxcx+w_a%!gcm4)u2@=_b2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ "c006.rodeo.tacc.utexas.edu", ".tapis.io",  ]
 
 
 # Application definition
