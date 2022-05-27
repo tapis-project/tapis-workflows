2ec6952f7d8952ef031becafbfb18bde9e7c8306
Nathan Freeman <nathan.dean.freeman@gmail.com>
2022-05-27 14:03:29 -0500

    configure api deployment to use scripts/run.sh on burnup

--- src/components/api/src/scripts/run.sh
+++ src/components/api/src/scripts/run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# python3 ./manage.py createsuperuser --no-input
-python3 ./manage.py runserver 0.0.0.0:$API_PORT
+
+python3 manage.py createsuperuser --no-input || true;
+python3 manage.py makemigrations;
+python3 manage.py migrate;
+python3 manage.py runserver 0.0.0.0:8000;
