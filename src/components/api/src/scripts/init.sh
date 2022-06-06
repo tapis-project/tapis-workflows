c8a9ab61d31801221d5628593ee8a03b80c68811
Nathan Freeman <nathan.dean.freeman@gmail.com>
2022-06-06 14:46:26 -0500

    create superuser after migrate

--- src/components/api/src/scripts/init.sh
+++ src/components/api/src/scripts/init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-# Uses the envrionment variables to resolve superuser username,
-# password, and email
-python3 manage.py createsuperuser --no-input || true;
-
 # Migrates the db schema
 chmod a+x ./scripts/migrate.sh
 ./scripts/migrate.sh
+
+# Uses the envrionment variables to resolve superuser username,
+# password, and email
+python3 manage.py createsuperuser --no-input || true;
