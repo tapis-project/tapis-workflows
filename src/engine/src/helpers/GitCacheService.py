b9d5f31b552f4c4ac1afb1fb0a9d31cc34f0dcbb
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-10-19 14:52:28 -0500

    fix directory construction git cache repo

--- src/engine/src/helpers/GitCacheService.py
+++ src/engine/src/helpers/GitCacheService.py
@@ -11,18 +11,14 @@ class GitCacheService:
         git.Repo.clone_from(url, os.path.join(self._cache_dir, directory.lstrip("/")))
 
     def repo_exists(self, path):
-        print("REPO EXISTS", os.path.exists(path))
-        return os.path.exists(path)
+        return os.path.exists(self._cache_dir, path.lstrip("/"))
     
     def update(self, directory):
-        print("GIT PULL", directory)
-        git.cmd.Git(directory).pull()
+        git.cmd.Git(self._cache_dir, directory.lstrip("/")).pull()
 
     def add_or_update(self, url, directory):
-        print("ADD OR UPDATE", directory)
         if not self.repo_exists(directory):
-            print("CREATING", url, directory)
             self.add(url, directory)
             return
-        print("UPDATING", directory)
+        
         self.update(directory)
