8bb245d25449ed7b886c61713c78a5e2015244bf
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-10-26 11:57:13 -0500

    use name eq main in function bootstrap

--- src/engine/src/helpers/function_bootstrap.py
+++ src/engine/src/helpers/function_bootstrap.py
@@ -1,5 +1,6 @@
 import os, sys
 
 
-sys.path.append("/mnt/open-workflow-engine/pipeline/task/src/owe_python_sdk")
-exec(open(os.environ.get("_OWE_ENTRYPOINT_FILE_PATH")).read())
+if __name__ == "__main__":
+    sys.path.append("/mnt/open-workflow-engine/pipeline/task/src/owe_python_sdk")
+    exec(open(os.environ.get("_OWE_ENTRYPOINT_FILE_PATH")).read())
