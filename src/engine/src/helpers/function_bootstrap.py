a67f11909c0cae340d5249f57c61a11efa43fe02
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-10-26 17:48:31 -0500

    update function bootstrap

--- src/engine/src/helpers/function_bootstrap.py
+++ src/engine/src/helpers/function_bootstrap.py
@@ -3,4 +3,5 @@ import os, sys
 
 if __name__ == "__main__":
     sys.path.append("/mnt/open-workflow-engine/pipeline/task/src")
+    os.chdir(os.path.dirname(os.environ.get("_OWE_ENTRYPOINT_FILE_PATH")))
     exec(open(os.environ.get("_OWE_ENTRYPOINT_FILE_PATH")).read())
