cf35f8f76a1aa209a6908e10b97410bd3e80893a
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-10-26 17:46:17 -0500

    test path append 1 dir up for sdk

--- src/engine/src/helpers/function_bootstrap.py
+++ src/engine/src/helpers/function_bootstrap.py
@@ -2,5 +2,5 @@ import os, sys
 
 
 if __name__ == "__main__":
-    sys.path.append("/mnt/open-workflow-engine/pipeline/task/src/owe_python_sdk")
+    sys.path.append("/mnt/open-workflow-engine/pipeline/task/src")
     exec(open(os.environ.get("_OWE_ENTRYPOINT_FILE_PATH")).read())
