e96ca93f0f97493f3da8754ae53493ba0b986280
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-10-26 19:44:04 -0500

    export path of task entrypoint dir in function bootstrap

--- src/engine/src/helpers/function_bootstrap.py
+++ src/engine/src/helpers/function_bootstrap.py
@@ -3,5 +3,6 @@ import os, sys
 
 if __name__ == "__main__":
     sys.path.append("/mnt/open-workflow-engine/pipeline/task/src")
+    sys.path.append(os.path.dirname(os.environ.get("_OWE_ENTRYPOINT_FILE_PATH")))
     os.chdir(os.path.dirname(os.environ.get("_OWE_ENTRYPOINT_FILE_PATH")))
     exec(open(os.environ.get("_OWE_ENTRYPOINT_FILE_PATH")).read())
