a12421fb812dd686d28319b7465d7db8b23b36cb
Nathan Freeman <nathan.dean.freeman@gmail.com>
2023-12-15 17:58:03 -0600

    remove old reactive state. improve template mapper mapping

--- src/engine/src/core/templating/TemplateMapper.py
+++ src/engine/src/core/templating/TemplateMapper.py
@@ -26,12 +26,17 @@ class TemplateMapper:
         }
         self.template_repo = TemplateRepository(cache_dir=cache_dir)
 
-    def map(self, obj: Union[Pipeline, Task], uses: Uses) -> Union[Pipeline, Task]:
+    def map(
+            self,
+            map_target: Union[Pipeline, Task],
+            uses: Uses
+        ) -> Union[Pipeline, Task]:
         """This method takes an object(Pipeline or Task object), and updates its
         attributes with the values found in the templates
         
         IMPORTANT NOTE:
-        The original object is modified and returned by this function, not a copy.
+        The map target object is modified and returned by this function, not a
+        copy.
         """
 
         # Clone git repository specified on the pipeline.uses if exists
@@ -41,51 +46,80 @@ class TemplateMapper:
             raise Exception(f"Template mapping error: {e}")
         
         # Resolve which class the final object should have
-        obj_class = Pipeline
-        if not issubclass(obj.__class__, Pipeline):
-            obj_class = self.task_map_by_type.get(template.get("type", None), None)
+        map_target_class = Pipeline
+        if not issubclass(map_target.__class__, Pipeline):
+            map_target_class = self.task_map_by_type.get(
+                template.get("type", None),
+                None
+            )
 
         # Raise exception if no class could be resolved from the template
-        if obj_class == None:
+        if map_target_class == None:
             raise Exception(f"Invalid Template: Unable to resolve object type from Template. Task template object 'type' property must be one of {list(self.task_map_by_type.keys())} | Recieved: {template.get('type', 'None')}")
 
-        dict_obj = obj.dict()
+        # This temporty object will hold the updated values for the final
+        # map target
+        tmp_obj = map_target.dict()
 
-        # Create a dictionary of the original object and map the properties of the template
-        # onto the dictionary
+        # Create a dictionary of the map target object and map the properties of
+        # the template onto the dictionary
         for attr in template.keys():
-            # For pipelines only. Skip the tasks property as they should be handled
-            # seperately in another call to the map method of the Template ampper
+            # For pipelines only. Skip the tasks property as they should be
+            # handled seperately in another call to the map method of the
+            # Template mapper
             if attr == "tasks":
                 continue
 
-            # For task only. The template should specify the correct type. For all other properties,
-            # that are not specified, we use that which in enumerated in the template
+            # For task only. The template should specify the correct type. For
+            # all other properties, that are not specified, we use that which in
+            # enumerated in the template
             if attr == "type":
-                dict_obj["type"] = template.get(attr)
+                tmp_obj["type"] = template.get(attr)
                 continue
-            
-            # Add the template's value for this attribute if the original object's
-            # value is not None, or if the object's og value is falsy and the templates value is not
-            if (
-                getattr(obj, attr, None) in [[], {}, "", None]
-                and template[attr] not in [[], {}, "", None]
-            ):
-                dict_obj[attr] = template[attr]
 
-        # Create a new object out of the modified dict representation of the original object
-        new_obj = obj_class(**dict_obj)
+            # Get the map target value and the template value for a given attr
+            map_target_value = getattr(map_target, attr, None)
+            template_value = template[attr]
 
-        # Now add all of the properties to the original object from the new object.
-        # NOTE this allows us to return the exact same object that was passed as and
-        # argument but with the modifications, there by maintaining the objects identity. 
+            # Default the tmp objects attr to the template's value for that attr
+            tmp_obj[attr] = template_value
+
+            # If map target value is None, set the tmp objects value to the
+            # templates value for the given attr
+            if map_target_value == None and template_value != None:
+                tmp_obj[attr] = template_value
+                continue
+
+            if template_value == None:
+                tmp_obj[attr] = map_target_value
+                continue
+
+            # TODO I do not thing this covers all cases well.
+            # # For all cases in which the object value is falsy and the template
+            # # value is NOT falsy, set the tmp object's attr to the template
+            # # object's value for that attr
+            # if (
+            #     map_target_value in [[], {}, ""]
+            #     and template_value not in [[], {}, ""]
+            # ):
+            #     tmp_obj[attr] = template_value
+
+        # Create a new object out of the modified dict representation of the
+        # map target object
+        new_obj = map_target_class(**tmp_obj)
+
+        # Now update all of the properties to the map target object from the tmp
+        # object.
+        # NOTE this allows us to return the exact same object that was passed as
+        # and argument but with the modifications, there by maintaining the
+        # objects identity. 
         for attr in new_obj.dict().keys():
             if attr == "tasks":
                 continue
 
             updated_value = getattr(new_obj, attr)
-            if getattr(obj, attr) != updated_value:
-                setattr(obj, attr, updated_value)
+            if getattr(map_target, attr) != updated_value:
+                setattr(map_target, attr, updated_value)
 
-        return obj
+        return map_target
 
