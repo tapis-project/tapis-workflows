b2fd8739c246f5b89c86a42af44d20f47d562d39
Nathan Freeman <nathan.dean.freeman@gmail.com>
2024-08-29 17:09:24 -0500

    with debug

--- src/api/src/backend/serializers/PipelineSerializer.py
+++ src/api/src/backend/serializers/PipelineSerializer.py
@@ -30,6 +30,8 @@ class PipelineSerializer:
             pipeline["tasks"] = [ TaskSerializer.serialize(t) for t in task_models ]
 
         # TODO Remove when certain these properties are not needed
+        print(type(pipeline_model.group))
+        print(pipeline_model.group)
         pipeline["group"] = UUIDSerializer.serialize(pipeline_model.group)
         # pipeline["current_run"] = UUIDSerializer.serialize(pipeline_model.current_run)
         # pipeline["last_run"] = UUIDSerializer.serialize(pipeline_model.last_run)
