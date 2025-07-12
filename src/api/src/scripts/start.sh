b9eeacc7fe993676667c9025c9421d797c30f66b
Nathan Freeman <nathan.dean.freeman@gmail.com>
2025-07-12 17:21:59 -0500

    Attempt to fix logging

--- src/api/src/scripts/start.sh
+++ src/api/src/scripts/start.sh
@@ -2,7 +2,7 @@
 
 chmod a+x ./scripts/init.sh
 ./scripts/init.sh
-uwsgi --http 0.0.0.0:8000 --module workflows.wsgi:application --master --die-on-term --logto /dev/stdout
+uwsgi --http 0.0.0.0:8000 --module workflows.wsgi:application --master --die-on-term --disable-logrotate --logto /dev/stdout
 # uvicorn workflows.asgi:application --host 0.0.0.0 --port 8000
 # gunicorn --bind 0.0.0.0:8000 --workers 3 workflows.wsgi --worker-class gevent --timeout 600
 # python3 manage.py runserver 0.0.0.0:8000;
