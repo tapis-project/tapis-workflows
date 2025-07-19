f03b1523b25734d494efa3b78156e648158a2803
Nathan Freeman <nathan.dean.freeman@gmail.com>
2025-07-19 17:15:33 -0500

    Fix baseurl resolver

--- src/engine/src/contrib/tapis/helpers/__init__.py
+++ src/engine/src/contrib/tapis/helpers/__init__.py
@@ -3,8 +3,11 @@ from tapipy.tapis import Tapis
 import jwt;
 
 def resolve_base_url_from_jwt(token: str):
+    # TODO The base url should NOT be derived from the token. it is possible
+    # that the authenticator may be sitting an another site. Therefore, we
+    # should proabably pass the base url through the args in the Workflows API
     decoded = jwt.decode(token, options={"verify_signature": False})
-    return decoded["iss"].removesuffix("")
+    return decoded["iss"].removesuffix("/v3/tokens")
 
 def new_tapis_client(jwt: str) -> Tapis:
     client = Tapis(
